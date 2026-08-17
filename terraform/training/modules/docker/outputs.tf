cf39c24f6e1340a396996119e98095b00b0b6f13
jafarulla15 <jafarulla15@gmail.com>
2026-08-17 19:59:26 +0600

    Docker installed

---
+++ terraform/training/modules/docker/outputs.tf
@@ -0,0 +1,9 @@
+output "server_ip" {
+  description = "Server IP where Docker was installed"
+  value       = var.server_ip
+}
+
+output "installation_status" {
+  description = "Docker installation status"
+  value       = "Docker installation completed"
+}
